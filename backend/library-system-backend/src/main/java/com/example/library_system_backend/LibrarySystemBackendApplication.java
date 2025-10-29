b0f5731f93343b2298a10d44d5ab5974ef7b43c6
safagurgen <safa.gurgen@gmail.com>
2025-10-29 19:19:32 +0300

    Initialize Spring Boot backend project

---
+++ backend/library-system-backend/src/main/java/com/example/library_system_backend/LibrarySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemBackendApplication.class, args);
+	}
+
+}
---
+++ backend/library-system-backend/src/test/java/com/example/library_system_backend/LibrarySystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
